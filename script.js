41bc2605f406b57e8630d1c17745655d8120a814
Bala Chandra Akula <115984987+BalaChandraAkula@users.noreply.github.com>
2023-03-21 09:43:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,60 @@
 //complete this code
+// class Person {
+// 	constructor(name,age){
+// 		this.name = name;
+// 		this.age = age;
+// 	}
+// 	set age(age){
+// 		return this.age = age;
+// 	}
+// 	get details(){
+// 		return this.name;
+// 	}
+// 	get age(){
+// 		return this.age;
+// 	}
+	
+// }
+
+// class Student extends Person {
+// 	study(){
+// 		console.log(`${this.name} is studying`)
+// 	}
+// }
+
+// class Teacher extends Person {
+// 	teach(){
+// 		console.log(`${this.name} is teaching`)
+// 	}
+// }
 class Person {
 	constructor(name,age){
-		this.name = name;
-		this.age = age;
+		this._name=name;
+		this._age=age;
 	}
+
+	get name(){
+		return this._name;
+	}
+
 	set age(age){
-		return this.age = age;
+		return this._age=age;
 	}
-	get details(){
-		return this.name;
-	}
-	get age(){
-		return this.age;
-	}
-	
+	get age() {
+	    return this._age;
+  }
 }
 
 class Student extends Person {
-	study(){
-		console.log(`${this.name} is studying`)
-	}
+	study() {
+    console.log(`${this._name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.name} is teaching`)
-	}
+	teach() {
+    console.log(`${this._name} is teaching`);
+  }
 }
 
 // Do not change the code below this line
